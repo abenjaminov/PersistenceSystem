680b14783b3897af3a3ba2813b689928f2866277
abenjaminov <abenjaminov@gmail.com>
2021-11-01 22:25:52 +0200

    Change the name for the module accessor class

--- Assets/Scripts/Persistence/ModuleAccessors/ModuleAccessor.cs
+++ Assets/Scripts/Persistence/ModuleAccessors/ModuleAccessor.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Persistence
 {
-    public abstract class BaseModuleAccessor : ScriptableObject
+    public abstract class ModuleAccessor : ScriptableObject
     {
         public abstract T GetData<T>(string key);
         public abstract void PersistData<T>(string key, T data);
