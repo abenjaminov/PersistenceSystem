37422af0e74cefacbb8074aae84ef57f264b0a83
abenjaminov <abenjaminov@gmail.com>
2021-11-01 22:39:33 +0200

    Update PlayerPersistenceModule.cs

--- Assets/Scripts/Persistence/Modules/PlayerPersistenceModule.cs
+++ Assets/Scripts/Persistence/Modules/PlayerPersistenceModule.cs
@@ -2,23 +2,26 @@ using DefaultNamespace;
 using Persistence;
 using UnityEngine;
 
-public class PlayerPersistenceModule : PersistenceModule
+namespace Persistence.Modules
 {
-    [SerializeField] private Player _player;
-        
-    public override void OnModuleLoaded(ModuleAccessor accessor)
+    public class PlayerPersistenceModule : PersistenceModule
     {
-        _player.Level = accessor.GetData<int>("PlayerLevel");
-        _player.Hp = accessor.GetData<int>("PlayerHp");
-        _player.transform.position = 
-            accessor.GetData<SerializableVector3>("PlayerPosition").ToVector3();
-    }
+        [SerializeField] private Player _player;
 
-    public override void OnModuleSaving(ModuleAccessor accessor)
-    {
-        accessor.PersistData<int>("PlayerLevel", _player.Level);
-        accessor.PersistData<int>("PlayerHp", _player.Hp);
-        accessor.PersistData<SerializableVector3>("PlayerPosition", 
-            SerializableVector3.GetSerializable(_player.transform.position));
+        public override void OnModuleLoaded(ModuleAccessor accessor)
+        {
+            _player.Level = accessor.GetData<int>("PlayerLevel");
+            _player.Hp = accessor.GetData<int>("PlayerHp");
+            _player.transform.position = 
+                accessor.GetData<SerializableVector3>("PlayerPosition").ToVector3();
+        }
+
+        public override void OnModuleSaving(ModuleAccessor accessor)
+        {
+            accessor.PersistData<int>("PlayerLevel", _player.Level);
+            accessor.PersistData<int>("PlayerHp", _player.Hp);
+            accessor.PersistData<SerializableVector3>("PlayerPosition", 
+                SerializableVector3.GetSerializable(_player.transform.position));
+        }
     }
 }
