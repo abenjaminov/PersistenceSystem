cd701c9c406637f65a49f18ab8c5dade95481098
abenjaminov <abenjaminov@gmail.com>
2021-11-01 22:45:10 +0200

    Update PersistenceModule.cs

--- Assets/Scripts/Persistence/Modules/PersistenceModule.cs
+++ Assets/Scripts/Persistence/Modules/PersistenceModule.cs
@@ -6,7 +6,6 @@ namespace Persistence.Modules
     public abstract class PersistenceModule : MonoBehaviour
     {
         public string Name;
-        public ModuleType ModuleType;
         public abstract void OnModuleLoaded(ModuleAccessor accessor);
         public abstract void OnModuleSaving(ModuleAccessor accessor);
     }
